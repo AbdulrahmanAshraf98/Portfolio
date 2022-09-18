e7534da742143a42241aa491b5124f907340be16
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2022-09-18 21:25:24 +0200

    Add Constant Data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { SKILLS } from "./utilities/Constant/Data/Skills/skills";
+
 function App() {
-	return <div>hello</div>;
+	return (
+		<div className="text-4xl font-bold font-signature">
+			<img src={SKILLS[0].icon__url} width="100px"></img>
+		</div>
+	);
 }
 
 export default App;
