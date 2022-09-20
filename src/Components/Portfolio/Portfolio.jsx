def79bdabafcf90d02eb142b8a7ad17587f4fee0
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2022-09-20 02:09:09 +0200

    ProtfolioComponent Add LazyLoadingImage lib

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -6,7 +6,7 @@ const Portfolio = () => {
 	const [Projects, setProjects] = useState(PROJECTS__DATA);
 	return (
 		<section
-			id=""
+			id="Portfolio"
 			className="portfolio py-16 bg-gradient-to-b from-gray-800 via-black to-black text-white">
 			<div className="container  m-auto h-full px-8  lg:px-16">
 				<div class="portfolio__content__title mb-12">
