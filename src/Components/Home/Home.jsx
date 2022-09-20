cd2136366c2edf973a5aca34fb94e96ac6f6a677
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2022-09-20 22:00:08 +0200

    Modfiy in padding sections

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 	return (
 		<section
 			id="Home"
-			className="hero-section text-5xl text-gray-800 w-full  bg-gradient-to-b from-black via-black to-gray-800  h-100 py-16 ">
+			className="hero-section text-5xl text-gray-800 w-full  bg-gradient-to-b from-black via-black to-gray-800  h-100 py-8 md:py-16 ">
 			<div class="container m-auto px-8 lg:px-16   h-full">
 				<div className="flex flex-wrap justify-between items-center h-full">
 					<div className="hero-section__content w-full md:w-6/12  text-gray-200 flex flex-col justify-center  order-2 md:order-1">
@@ -15,20 +15,26 @@ const Home = () => {
 							I am Front End Developer
 						</h2>
 						<p className="text-lg text-gray-500 py-8 max-w-2xl">
-							I am passionate frontend developer from Egypt I Graduated from
-							faculty of commerce department of Business information systems,
-							Helwan university (2016- 2020) with Very Good grade.Seeking a job
-							in the field of “Front End Development” in a reputable
-							organization where gain experience. Eager to learn every day and
-							gain more experience in different fields related to technology
+							I am a frontend web developer. I can provide clean code and pixel
+							perfect design. I also make website more & more interactive with
+							web animations.
 						</p>
-						<div>
-							<button className="group text-xl text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+						<div className="flex flex-wrap gap-4">
+							<a
+								href="#Portfolio"
+								className="group text-lg sm:text-xl text-white w-fit px-3 sm:px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
 								Portfolio
 								<span class="group-hover:rotate-90 duration-300">
 									<MdKeyboardArrowRight size={25} />
 								</span>
-							</button>
+							</a>
+							<a
+								href="https://drive.google.com/file/d/1sH2p0cL4JNsoGgraF_zuOYBk2qHikslY/view?usp=sharing"
+								target="_blank"
+								rel="noopener noreferrer"
+								className="group text-lg sm:text-xl text-white w-fit px-10 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-blue-500 via-blue-500 to-cyan-500 cursor-pointer">
+								Cv
+							</a>
 						</div>
 					</div>
 					<div className="hero-section__image w-full md:w-5/12   order-1 md:order-2 mb-8 md:mb-0">
--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -6,7 +6,7 @@ function Resume() {
 	return (
 		<section
 			id="Resume"
-			className="resume  py-16 w-full bg-gradient-to-b from-black via-gray-800 to-black text-white">
+			className="resume  py:8 md:py-16 w-full bg-gradient-to-b from-black via-gray-800 to-black text-white">
 			<div className="container  m-auto h-full px-8  lg:px-16 ">
 				<div className="flex flex-wrap justify-between items-start gap-1">
 					<div className="eduction w-full lg:w-6/12 mb-8  lg:mb-0">
